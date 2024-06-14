23c60ea3a42a7eb7d1f1e4874b910cc71c842db1
ayeshasingh21 <166839738+ayeshasingh21@users.noreply.github.com>
2024-06-14 15:57:18 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-fetch('https://reqres.in/api/users?page=1')
-.then(res=>res.json())
-.then(result=>{
-    console.table(result['data'])
-    output=''
-    for( let i=0;i<result['data'].length;i++)
-    {
-    user=result['data'][i]
-    output+=`<tr>
-            <td>${user.id}</td>
-            <td>${user.email}</td>
-            <td>${user.first_name}</td>
-            <td>${user.last_name}</td>
-            <td><img src='${user.avatar}"></td>
-        </tr>`
-}
- 
-  document.getElementById('records').innerHTML=output
-})
+fetch('https://reqres.in/api/users?page=2')
+        .then(res=>res.json())
+        .then(result=>{
+            output=''
+            for(let i=0;i<result['data'].length;i++){
+                user=result['data'][i]
+                output+=` <tr>
+                    <td>${user.id}</td>
+                    <td>${user.email}</td>
+                    <td>${user.first_name}</td>
+                    <td>${user.last_name}</td>
+                    <td><td><img src="${user.avatar}" alt=""></td></td>`
+            }
+            console.table(result['data'])
+            document.getElementById('records').innerHTML = output
+        })
